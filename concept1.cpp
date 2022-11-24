17ed83156544094752e29c669be5fff130c15cbd
Subhamraj1510 <118970683+Subhamraj1510@users.noreply.github.com>
2022-11-24 15:40:41 +0530

    Add files via upload

---
+++ concept1.cpp
@@ -0,0 +1,16 @@
+// classes and objects 
+#include <iostream>  
+using namespace std;  
+class Student {  
+   public:  
+      int id;//data member (also instance variable)      
+      string name;//data member(also instance variable)      
+};  
+int main() {  
+    Student s1; //creating an object of Student   
+    s1.id = 21051440;    
+    s1.name = "Subham Yadav";   
+    cout<<s1.id<<endl;  
+    cout<<s1.name<<endl;  
+    return 0;  
+}  
